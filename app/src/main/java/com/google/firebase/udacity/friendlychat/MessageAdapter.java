e044f68b7c1779ae9730f6ad212d7aac73b006ec
MahmoudAbdullahGITHUB <beso@MAHMOUD.com>
2019-11-27 21:40:14 +0200

    Initial commit

---
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MessageAdapter.java
@@ -0,0 +1,54 @@
+package com.google.firebase.udacity.friendlychat;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
+    public MessageAdapter(Context context, int resource, List<FriendlyMessage> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
+        }
+
+        ImageView photoImageView = (ImageView) convertView.findViewById(R.id.photoImageView);
+        TextView messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
+        TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+
+        FriendlyMessage message = getItem(position);
+        System.out.println("___________________________________________");
+        System.out.println("message = "+message.getName());
+        System.out.println("message = "+message.getPhotoUrl());
+
+        boolean isPhoto = message.getPhotoUrl() != null;
+        System.out.println("isPhoto = "+isPhoto);
+        if (isPhoto) {
+            System.out.println("isPhoto = TTTTT");
+            messageTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(photoImageView.getContext())
+                    .load(message.getPhotoUrl())
+                    .into(photoImageView);
+        } else {
+            System.out.println("isPhoto = FFFFF");
+            messageTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            messageTextView.setText(message.getText());
+        }
+        authorTextView.setText(message.getName());
+
+        return convertView;
+    }
+}
